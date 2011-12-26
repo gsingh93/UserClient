81f2023a64c5170c792c35f55c43068122b150ac
Gulshan Singh <gulshan@umich.edu>
2011-12-25 22:30:54 -0500

    initial commit

---
+++ src/gsingh/busapp/BusAppActivity.java
@@ -0,0 +1,47 @@
+package gsingh.busapp;
+
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.maps.MapActivity;
+
+/*
+ * To test in the emulator, use the telnet commands:
+ * telnet localhost 5554
+ * geo fix 30.0 30.0
+ */
+
+public class BusAppActivity extends MapActivity {
+
+	private LocationManager locManager = null;
+	private LocationListener locListener = null;
+	private TextView display = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		display = (TextView) findViewById(R.id.textview);
+
+		/* Use the LocationManager class to obtain GPS locations */
+		locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locListener = new MyLocationListener(display);
+	}
+
+	public void onClickLocate(View v) {
+		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+				locListener);
+		display.setText("Locating...");
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ src/gsingh/busapp/MyLocationListener.java
@@ -0,0 +1,106 @@
+package gsingh.busapp;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MyLocationListener implements LocationListener {
+
+	/**
+	 * URL object of server
+	 */
+	URL url;
+
+	/**
+	 * Address of server
+	 */
+	String URLText = null;
+	
+	/**
+	 * Information TextView
+	 */
+	TextView display = null;
+
+	MyLocationListener(TextView display) {
+		super();
+		this.display = display;
+	}
+
+	@Override
+	public void onLocationChanged(Location loc) {
+		double lat = loc.getLatitude();
+		double lon = loc.getLongitude();
+
+		// Create text to be displayed on screen
+		String Text = "My current location is: " + "\nLatitude = " + lat
+				+ "\nLongitude = " + lon;
+
+		// Display location
+		display.setText(Text
+				+ "\n\nCheck http://michigangurudwara.com/bus.php to see your location");
+
+		// Send location to server
+		URLText = "http://michigangurudwara.com/bus.php?lat=" + lat + "&lon="
+				+ lon;
+
+		// Set up HTTP objects
+		DefaultHttpClient hc = new DefaultHttpClient();
+		ResponseHandler<String> res = new BasicResponseHandler();
+		HttpPost postMethod = new HttpPost(URLText);
+
+		// TODO: Is the below necessary?
+		// Create data object
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		nameValuePairs.add(new BasicNameValuePair("lat", String.valueOf(lat)));
+		nameValuePairs.add(new BasicNameValuePair("lon", String.valueOf(lon)));
+
+		// Send data
+		try {
+			postMethod.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			String response = hc.execute(postMethod, res);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+}
